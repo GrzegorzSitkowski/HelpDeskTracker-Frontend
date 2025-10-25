ad181e37b9c9007a8ead650b3abeae58fd5d1f9a
GrzegorzSitkowski <sitkowskigrzegorz8@gmail.com>
2025-10-25 19:46:58 +0200

    Installed packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  imports:{
+    dirs: ['stores'],
+  },
+
+  modules: [
+    '@pinia/nuxt',
+    '@vueuse/nuxt',
+    'nuxt-lodash',
+    'dayjs-nuxt'
+  ],
+
+  lodash: {
+    prefix: "_",
+    prefixSkip: false,
+    upperAfterPrefix: false,
+  },
 })
